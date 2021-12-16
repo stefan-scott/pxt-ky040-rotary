b37e56b9ed018c002ee98e914b19f97f7d75bf93
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-12-16 00:44:02 +0000

    use large image with blank space either side and variable to scroll image

--- main.ts
+++ main.ts
@@ -1,19 +1,22 @@
 KY040.OnPinPressed(DigitalPin.P0, function () {
-    basic.showIcon(IconNames.Yes)
-    basic.pause(100)
-    basic.clearScreen()
+    Xcord = 2
 })
 KY040.onTurned(direction.clockwise, function () {
-    basic.showArrow(ArrowNames.West)
-    basic.pause(100)
-    basic.clearScreen()
+    Xcord = Xcord + 1
 })
 KY040.onTurned(direction.counterclockwise, function () {
-    basic.showArrow(ArrowNames.East)
-    basic.pause(100)
-    basic.clearScreen()
+    Xcord = Xcord - 1
 })
+let Xcord = 0
 KY040.setKY040(DigitalPin.P15, DigitalPin.P1)
+Xcord = 2
+basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-	
+    images.createBigImage(`
+        . . . # . # . . . .
+        . . # # # # # . . .
+        . . # # # # # . . .
+        . . . # # # . . . .
+        . . . . # . . . . .
+        `).showImage(Xcord, 400)
 })
