76d08fabcc66d8fa81810532f9be7dd76625279d
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-02-28 21:59:31 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-KY040.OnPinPressed(DigitalPin.P0, function () {
+KY040.OnPinPressed(DigitalPin.P14, function () {
     Xcord = 2
 })
 KY040.onTurned(direction.clockwise, function () {
@@ -8,7 +8,7 @@ KY040.onTurned(direction.counterclockwise, function () {
     Xcord = Xcord - 1
 })
 let Xcord = 0
-KY040.setKY040(DigitalPin.P15, DigitalPin.P1)
+KY040.setKY040(DigitalPin.P1, DigitalPin.P15)
 Xcord = 2
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
