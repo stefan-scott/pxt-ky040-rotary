c481e733f11b84d56bb91002c588e6d5997095bf
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-02-28 22:02:14 +0000

    update 040 extension

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-KY040.OnPinPressed(DigitalPin.P14, function () {
+KY040.onPressEvent(function () {
     Xcord = 2
 })
 KY040.onTurned(direction.clockwise, function () {
@@ -8,7 +8,7 @@ KY040.onTurned(direction.counterclockwise, function () {
     Xcord = Xcord - 1
 })
 let Xcord = 0
-KY040.setKY040(DigitalPin.P1, DigitalPin.P15)
+KY040.setKY040(DigitalPin.P1, DigitalPin.P15, DigitalPin.P14)
 Xcord = 2
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
